3352a0a05da02493bc786e5a542cffa1ca63db48
Alexis Moraga <alexis.moraga.gallardo@gmail.com>
2018-03-29 10:31:40 -0300

    clases 20 y 21 habland de ECMAScript y Babel

---
+++ Clase20-21-ECMAScript_y_Babel.js
@@ -0,0 +1,33 @@
+
+/*ECMAScript: El estándar en el que se basa JavaScript*
+
+El estándar en el que se basa JavaScript.
+
+Comenzó llamándose LiveScript para lo que se mostraba al cliente.
+
+Java != JavaScript
+
+Se llamó ECMAScript porque fue desarrollado por ECMA, una empresa dedicada a telecomunicaciones.
+
+ECMA-262
+
+Sus primeras versiones fueron lanzadas en los 90’s.
+
+En este momento se está creando ECMAScript 2018.
+
+Si escribimos código con ECMAScript tiene que ser compatible con el navegador en el que queremos que corra.
+
+En el siguiente vamos a hablar sobre BABEL.
+*/
+
+
+
+/*Babel al rescate: logrando la compatibilidad buscada
+Babel
+
+Usa JavaScript de la próxima generación hoy mismo.
+
+Babel es una herramienta que te permite hacer compatible el código que usa funcionalidades de ECMAScript con los diferentes navegadores.
+
+Para poder utilizar Babel debes tener instalado Node.
+*/
